62feea361fafd586bc148a1c253107c04e3398a3
1345jose <josevas1347@gmail.com>
2025-09-24 22:38:25 -0500

    espacio

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable
         'email',
         'password',
         'nombre_completo',
-        'documento  ',
+        'documento',
         'telefono',
         'apellidos',
     ];
